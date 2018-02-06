599eb6b3bf43ba852cf770367ba052a48948f9c2
Markus Maelzer <markus.maelzer@gmail.com>
2018-02-06 09:19:34 +0100

    added send to external server

--- app.js
+++ app.js
@@ -3,24 +3,39 @@ const request = require('request');
 const cmd = require('node-cmd');
 const fs = require('fs');
 
+const io = require('socket.io-client');
+const socket = io.connect('http://c81ad749.ngrok.io', { reconnection: true});
+
+socket.on('connect', () => {
+  console.log('connected');
+})
+
 var cam = new RaspiCam({
   mode: "photo",
   output: `${__dirname}/image/plate.jpg`,
   encoding: "jpg",
-  timeout: 0,
+  timeout: 5000,
   width: '1920',
-  height: 1080
+  height: 1080,
 });
+// max-w: 3280
+// max-h: 2464
 
-setInterval(() => {
-  cam.start();
-}, 10000);
-cam.start();
+
+function startCam(timeout) {
+  setTimeout(function () {
+    cam.start();
+  }, timeout)
+}
+startCam(5000);
+
+
 // command[0] -> RASPBERRY
 // command[1] -> windows
 const command = [`alpr -c eu -j image/plate.jpg`, `cd openalpr_64 && alpr -c eu -j samples/aut-5.jpg`];
 
 cam.on('read', (err, timestamp, filename) => {
+  console.log('read');
   getPlate();
 })
 
@@ -31,36 +46,18 @@ function getPlate() {
       var parsedData = JSON.parse(data);
       console.log('Data:', parsedData);
       if(parsedData.results.length > 0) {
-        console.log('Plate:', parsedData.results[0].plate);
+        const { plate } = parsedData.results[0];
+        console.log('Plate:', plate);
+        startCam(30000);
+        socket.emit('detection-success', {message: 'Sucess', data: parsedData, plate });
       } else {
-        request.post({url: ''})
+        console.log('error');
+        // request.post({url: ''})
+        startCam(10000);
+        fs.readFile('image/plate.jpg', (err, image) => {
+          socket.emit('detection-error-image', { message: "No Plate detected", alprData: parsedData, image});
+        })
       }
     }
   );
 }
-
-// getPlate();
-// var test;
-// // bims zu bad um function working zu mochn :c
-// const getPlate = () => {
-//   cmd.get(
-//     `cd openalpr_64 && alpr -c eu -j samples/aut-5.jpg`,
-//
-//     function (err, data, stderr) {
-//       console.log(JSON.parse(data));
-//       res.send(JSON.parse(data.results))
-//       // write test data to test.txt
-//       // fs.appendFile('test.txt', data, function (err) {
-//       //   console.log(err);
-//       // })
-//     }
-//   )
-// }
-
-
-
-/*
-try out creating objects with the itlab verwaltung/stundenplan
-twoarray sth function
-
-*/
