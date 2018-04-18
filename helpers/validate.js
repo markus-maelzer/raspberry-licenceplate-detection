c3580677029495bfd009b65893a40db013ee5c09
Markus <markus.maelzer@gmail.com>
2018-04-19 00:41:01 +0200

    .

--- helpers/validate.js
+++ helpers/validate.js
@@ -7,13 +7,32 @@ class Validator {
     this.aForDelete = 100;
   }
 
-  newPlate(plate) {
-    if(plates[plate]) {
-      plates[plate].count += 1;
+  setPlate(plate) {
+    if(!plate) return;
+    if(this.plates[plate]) {
+      this.checkPlate(plate, this.plates[plate].countPlate.next());
     }
     else {
-
+      this.plates[plate] = new Plate(this.aForSend, this.aForDelete);
+      this.plates[plate].init();
+      this.addToTotalCount();
     }
+  }
+
+  checkPlate(plate, {value, done}) {
+    // console.log('Value-Done', value, done);
+    // console.log('Plate: ', plate);
+    // console.log('Plates: ', this.plates);
+    if(done){
+      delete this.plates[plate];
+      return false;
+    };
+  }
+
+  addToTotalCount() {
+    _.forIn(this.plates, (value, key) => {
+
+    })
   }
 }
 
@@ -23,8 +42,6 @@ class Plate {
     this.totalCount = 1;
     this.aForSend = aForSend || 10;
     this.aForDelete = aForDelete || 100;
-
-    this.plateCountUp = this.plateCountUp.bind(this)
   }
   * plateCountUp() {
     while(this.plateCount < this.aForSend)
@@ -32,25 +49,29 @@ class Plate {
   }
   * totalCountUp() {
     while(this.totalCount < this.aForDelete)
-      yield this.aForDelete++;
+      yield this.totalCount++;
   }
-  test() {
-    console.log(this.plateCount);
+  init() {
+    this.countPlate = this.plateCountUp();
+    this.countTotal = this.totalCountUp();
   }
 }
 
-const test = new Plate();
-const test2 = test.totalCountUp();
-console.log(test2.next());
-console.log(test2.next());
-console.log(test2.next());
-console.log(test2.next());
-console.log(test2.next());
+const validator = new Validator();
 
-console.log(test2.next());
-console.log(test2.next());
-console.log(test2.next());
-console.log(test2.next());
-console.log(test2.next());
-test.test()
-console.log(test2.next());
+const plates2 = [
+  'test1', 'test2', 'test3', 'test4', 'test5',
+  'test6', 'test7', 'test8', 'test9', 'test10',
+  'test11', 'test12', 'test13', 'test14', 'test15',
+  'test16', 'test17', 'test18', 'test19', 'test20'
+]
+
+setInterval(function () {
+  validator.setPlate(plates2[Math.floor(Math.random() * 5)]);
+}, 500);
+
+// make it count for all plates on totalCount
+
+// send nudes when countPlate() returns true
+
+// fix delete shit >_>
